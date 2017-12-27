5a0b18ac49a902d17ac36ffa6b3061a36ca9acfc
Light <bpk395@mail.ru>
2017-12-27 17:04:23 +0700

    2

--- app/src/main/java/bpk/light/app_10/MainActivity.java
+++ app/src/main/java/bpk/light/app_10/MainActivity.java
@@ -3,6 +3,8 @@ package bpk.light.app_10;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 
@@ -10,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     RadioButton rbtnNum, rbtnPass, rbtnName, rbtnMap, rbtnCont;
     CheckBox chkMap, chkCont;
     SeekBar seekNumVar;
+    EditText editNumVar, editNumMin, editNumMax;
+    LinearLayout layNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +26,10 @@ public class MainActivity extends AppCompatActivity {
         chkMap = (CheckBox) findViewById(R.id.chkMap);
         chkCont = (CheckBox) findViewById(R.id.chkCont);
         seekNumVar = (SeekBar) findViewById(R.id.seekNumVar);
+        editNumVar = (EditText) findViewById(R.id.editNumVar);
+        editNumMin = (EditText) findViewById(R.id.editMinNum);
+        editNumMax = (EditText) findViewById(R.id.editMaxNum);
+        layNum = (LinearLayout) findViewById(R.id.layNum);
+
     }
 }
